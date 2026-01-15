1845fcd52adb0f5f4d4f4ed34628dce431c13c6f
peny <petter@pttr.se>
2026-01-15 20:57:14 +0100

    Update date to '10:e juli 2026'

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ function Hero() {
       <div className="hero-content">
         <h1 className="hero-title">Petter och Emma gifter sig!</h1>
         <div className="hero-subtitle">Save the Date</div>
-        <div className="hero-date">10 juli 2026</div>
+        <div className="hero-date">10:e juli 2026</div>
       </div>
       <div className="hero-overlay"></div>
     </section>
