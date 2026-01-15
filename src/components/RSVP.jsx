0d2f19753b7d5550136b6a121ddd7a734f97a107
peny <petter@pttr.se>
2026-01-15 21:17:01 +0100

    Keep RSVP button text on one line

--- src/components/RSVP.jsx
+++ src/components/RSVP.jsx
@@ -14,8 +14,7 @@ function RSVP() {
             rel="noopener noreferrer"
             className="rsvp-button"
           >
-            O.S.A här senast 1 maj
-            <span className="rsvp-button-arrow">→</span>
+            <span style={{whiteSpace: 'nowrap'}}>O.S.A&nbsp;här&nbsp;senast&nbsp;1&nbsp;maj <span className="rsvp-button-arrow">→</span></span>
           </a>
         </div>
       </div>
